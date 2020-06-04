dafd604be864d9018722f2ab387da5bbe4835151
Sabreen Noah <sabreen.noah@gmail.com>
2020-06-04 12:48:51 -0700

    not fully covered

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,12 @@
-/*
 import Index from "./index";
 
-test('test uncovered if', () => {
+test("test uncovered if", () => {
     const indexObj = new Index();
     expect(indexObj.uncovered_if()).toEqual(false);
 });
-
+/*
 test('fully covered', () => {
     const indexObj = new Index();
     expect(indexObj.fully_covered()).toEqual(true);
 });
 */
-
-test("this should pass but tests nothing :(", () => {
-    return;
-});
